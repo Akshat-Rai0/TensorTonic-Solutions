8ee5871b0e6795cc1931183c6a7c4fccb4a89705
Akshat rai <arai34752@gmail.com>
2026-06-15 16:09:14 +0530

    Solve text-chunking

---
+++ text-chunking/text-chunking.py
@@ -0,0 +1,13 @@
+def text_chunking(tokens, chunk_size, overlap):
+    step = chunk_size - overlap
+    result = []
+
+    for i in range(0, len(tokens), step):
+        chunk = tokens[i:i + chunk_size]
+
+        if len(chunk) < chunk_size and i != 0:
+            break
+
+        result.append(chunk)
+
+    return result
